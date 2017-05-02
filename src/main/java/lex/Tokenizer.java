b3ac79cacc4df22a572e5b78b9e398fa05727f60
Isabella <isabellacuba@Isabellas-MBP.fios-router.home>
2017-05-01 23:24:49 -0400

    fixed the Tokenized
    finished the tokenizer and all helper functions

--- src/main/java/lex/Tokenizer.java
+++ src/main/java/lex/Tokenizer.java
@@ -1,7 +1,10 @@
 package lex;
 
-import com.sun.javafx.fxml.expression.Expression;
+
 import errors.LexicalError;
+import symbolTable.KeywordTable;
+import symbolTable.SymbolTable;
+import symbolTable.SymbolTableEntry;
 
 
 import java.io.File;
@@ -106,12 +109,134 @@ public class Tokenizer
 
 	}
 
-	private Token readDigit(Character seen) throws LexicalError{
+	/** todo **/
+	//---------------------
+    //readDigit(Character seen) --> Token
+    //      returns the appropriate token (either intconstant or realconstant) based
+    //      on the characters seen
+    //---------------------
+	private Token readDigit(Character seen) throws LexicalError {
+        StringBuffer numbers = new StringBuffer();
+        while (Character.isDigit(seen)) {
+            numbers.append(Character.toString(seen));
+            seen = stream.currentChar();
+        }
+        if (seen == '.') {
+            char lookahead = stream.currentChar();
+            if (Character.isDigit(lookahead)) {
+                numbers.append(Character.toString(lookahead));
+                do {
+                    numbers.append(Character.toString(lookahead));
+                    lookahead = stream.currentChar();
+                } while (Character.isDigit(lookahead));
+                // we might have exponents:
+                char lookForExp = stream.currentChar();
+                //if we see an E in the stream,
+                if (Character.toString(lookForExp).toUpperCase().equals("E")) {
+                    if (lookForExp == '+' || lookForExp == '-' || Character.isDigit(lookForExp)) {
+                        char lookahead3 = stream.currentChar();
+                        if (Character.isDigit(lookahead3)) {
+                            // we have a real number exponent, so we should add the exponant digits.
+                            numbers.append(Character.toString(lookahead));
+                            numbers.append(Character.toString(lookForExp));
+                            numbers.append(Character.toString(lookahead3));
+                            lookahead3 = stream.currentChar();
+                            while (Character.isDigit(lookahead3)) {
+                                numbers.append(Character.toString(lookahead3));
+                                lookahead3 = stream.currentChar();
+                            }
+                            stream.pushBack(lookahead3);
+                            t.setType(TokenType.REALCONSTANT);
+                            t.setValue(numbers.toString());
+                        } else {
+                            // it is not a digit and we push back the non-digit we looked at
+                            stream.pushBack(lookahead3);
+                            stream.pushBack(lookForExp);
+                            stream.pushBack(lookahead);
+                            t.setType(TokenType.REALCONSTANT);
+                            t.setValue(numbers.toString());
+                        }
+                    } else {
+                        // The lookahead id not  +,-, or DIGIT so the E must be part of something else.
+                        //Push back the lookaheads
+                        stream.pushBack(lookForExp);
+                        stream.pushBack(lookahead);
+                        t.setType(TokenType.REALCONSTANT);
+                        t.setValue(numbers.toString());
+                    }
+                } else {
+                    // what we see must not be E or DIGIT, so push it back
+                    stream.pushBack(lookForExp);
+                    stream.pushBack(lookahead);
+                    // make the token a REAL
+                    t.setType(TokenType.REALCONSTANT);
+                    t.setValue(numbers.toString());
+                }
+
+            } else {
+                // The . is not followed by a numbersber, it is a part of something else.
+                // push back the lookahead and make the token an integer
+                stream.pushBack(lookahead);
+                stream.pushBack(seen);
+                t.setType(TokenType.INTCONSTANT);
+                t.setValue(numbers.toString());
+            }
+
+        }
+        // we have not seen a period (.)
+        else {
+            //Check for an E for exponants.
+                //if there is an exponent, then it's a real
+                // 2a. MAYBE EXPONENT
+            if (((Character.toString(seen)).toUpperCase().equals("E"))) {
+                // when we see the E, we need a lookahead to gether the rest of the digits
+                char lookahead = stream.currentChar();
+                //if we see +, - or Digit
+                if (lookahead == '+' || lookahead == '-' || Character.isDigit(lookahead)) {
+                    //check if the number is an exponent
+                    char lookForExp = stream.currentChar();
+                    if (Character.isDigit(lookForExp)) {
+                        //found a number with an exponent
+                        numbers.append(Character.toString(seen));
+                        numbers.append(Character.toString(lookahead));
+                        numbers.append(Character.toString(lookForExp));
+                        lookForExp = stream.currentChar();
+                        while (Character.isDigit(lookForExp)) {
+                            numbers.append(Character.toString(lookForExp));
+                            lookForExp = stream.currentChar();
+                        }
+                        stream.pushBack(lookForExp);
+                        t.setType(TokenType.REALCONSTANT);
+                        t.setValue(numbers.toString());
+                    } else {
+                        stream.pushBack(lookForExp);
+                        stream.pushBack(lookahead);
+                        stream.pushBack(seen);
+                        t.setType(TokenType.INTCONSTANT);
+                        t.setValue(numbers.toString());
+                    }
+                } else {
+                    // the next character is not + - or DIGIT, so the E must be the start of something else.
+                    stream.pushBack(lookahead);
+                    stream.pushBack(seen);
+                    t.setType(TokenType.INTCONSTANT);
+                    t.setValue(numbers.toString());
+                }
+            } else {
+                //There is no Exponant so push the lookaheads back and make it a constant
+                stream.pushBack(seen);
+                t.setType(TokenType.INTCONSTANT);
+                t.setValue(numbers.toString());
+            }
+        }
+        return t;
+    }
 
 
-	}
+
 
 	//------------------------
+    //readLetter(Character seen) --> Token
     //   Makes a StringBuffer of all the letters found next to each other, and calls a sister
     //   Function which identifies the proper Token Type for the string found and sets the
     //   Token type accordingly;
@@ -137,11 +262,49 @@ public class Tokenizer
     //    Takes a string and determines whether it is an identifier or
     //    keyword, and sets the token type accordingly.
 	private Token whichTokenType(String str) {
+        String upperID = str.toUpperCase();
+        SymbolTableEntry s = keywordTable.lookup(upperID);
+        if (s != null) {
+            switch(upperID) {
+                case "AND":
+                    t.setType(s.getType());
+                    t.setOpType(Token.OperatorType.AND);
+                    break;
+                case "OR":
+                    t.setType(s.getType());
+                    t.setOpType(Token.OperatorType.OR);
+                    break;
+                case "NOT":
+                    t.setType(s.getType());
+                    t.setOpType(Token.OperatorType.NOT);
+                    break;
+                case "DIV":
+                    t.setType(s.getType());
+                    t.setOpType(Token.OperatorType.INTEGERDIVIDE);
+                    break;
+                case "MOD":
+                    t.setType(s.getType());
+                    t.setOpType(Token.OperatorType.MOD);
+                    break;
+                default:
+                    t.setType(s.getType());
+                    break;
+            }
+
+        } else {
+            // make the identifier (it's not a keyword)
+            t.setType(TokenType.IDENTIFIER);
+            t.setValue(str);
+        }
+        return t;
 
     }
 
+    private boolean isMulop(Character ch) {
+	    return (ch == '*' || ch == '/');
+    }
 
-	private Token readMulOp(Character seen) throws LexicalError {
+	private Token readMulOp(char seen) throws LexicalError {
         switch (seen) {
             case '*':
                 t.setType(TokenType.MULOP);
@@ -154,6 +317,10 @@ public class Tokenizer
 
         }
 	}
+
+	private boolean isAddOp(char ch) {
+	    return ( ch == '+' || ch == '-');
+    }
 
 	private Token readAddOp(Character seen) throws LexicalError{
         TokenType lastTokenType = getLastToken().getType();
@@ -189,6 +356,10 @@ public class Tokenizer
         return t;
 	}
 
+	private boolean isRelopOrAssign(char ch) {
+        return ch == '=' || ch == '>' || ch == '<';
+    }
+
 	private Token readRelOp(Character seen) throws LexicalError{
 	    char nextChar;
 	    switch (seen) {
@@ -204,7 +375,7 @@ public class Tokenizer
                 } else {
                     t.setOpType(Token.OperatorType.GREATERTHAN);
                     t.setType(TokenType.RELOP);
-                    stream.pushBack(lookahead);
+                    stream.pushBack(nextChar);
                 }
                 break;
 
@@ -273,8 +444,8 @@ public class Tokenizer
                 break;
             default:
                 // If it isn't valid... throw an error
-                if (!stream.valid(ch)) {
-                    throw LexicalError.IllegalCharacter(ch, stream.lineNumber());
+                if (!stream.valid(seen)) {
+                    throw LexicalError.IllegalCharacter(seen, stream.lineNumber());
                 }
         }
         return t;
